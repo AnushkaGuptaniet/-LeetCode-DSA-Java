e1d8b4e8fb050fae4f90b392b334685f03625467
Anushka Gupta <anushkagupta4503@gmail.com>
2026-04-03 10:54:31 +0530

    Finding middle element.java

--- Linked List/Finding middle element.java
+++ Linked List/Finding middle element.java
@@ -26,3 +26,19 @@ class Solution {
     }
 }
 // TC:O(n), SC:_O(1)
+
+
+
+ // Approach 2:- slow and fast pointer 
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }   
+        return slow;
+    }
+}
+// TC:O(n), SC:_O(1)
