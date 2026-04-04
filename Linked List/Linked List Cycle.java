c36f73208bd587059279fab9121785d177737a5b
Anushka Gupta <anushkagupta4503@gmail.com>
2026-04-04 23:33:44 +0530

    Linked List Cycle.java

---
+++ Linked List/Linked List Cycle.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+ // Approach 1:- brute force(hashing)
+
+ public class Solution {
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+        ListNode temp = head;
+        while(temp != null){
+            if(set.contains(temp)){
+                return true;
+            }
+            set.add(temp);
+            temp = temp.next;
+        }
+        return false;
+    }
+ }
+
+
+
+ // Approach : - optimized(slow and fast pointer)
+// public class Solution {
+//     public boolean hasCycle(ListNode head) {
+//         if (head == null) return false;
+
+//         ListNode slow = head;
+//         ListNode fast = head;
+
+//         while (fast != null && fast.next != null) {
+//             slow = slow.next;
+//             fast = fast.next.next;
+//             if (slow == fast) return true;
+//         }
+//         return false;
+//     }
+// }
+// TC:- O(N), SC:_O(1)
