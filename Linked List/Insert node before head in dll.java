231d1fa95d8f21825779c9b71d351a10aaa5f3d0
Anushka Gupta <anushkagupta4503@gmail.com>
2026-04-05 00:19:30 +0530

    Insert node before head in dll.java

---
+++ Linked List/Insert node before head in dll.java
@@ -0,0 +1,22 @@
+ /*
+// Definition for a Node.
+class ListNode {
+    public int data;
+    public ListNode prev;
+    public ListNode next;
+    public ListNode();
+    public ListNode(int data);
+    public ListNode(int data, ListNode prev, ListNode next);
+};
+*/
+
+class Solution {
+    public ListNode insertBeforeHead(ListNode head, int data) {
+        ListNode temp = new ListNode(data);
+        temp.next = head;
+        if(head != null){
+            head.prev = temp;
+        }
+        return temp;
+    }
+}
