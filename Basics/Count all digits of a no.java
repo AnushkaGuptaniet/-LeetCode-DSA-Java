bdae601d1b348324bee6b7ad6c5e0bedaf7f61fb
Anushka Gupta <anushkagupta4503@gmail.com>
2026-04-13 19:30:16 +0530

    Count all digits of a no

--- Basics/Count all digits of a no.java
+++ Basics/Count all digits of a no.java
@@ -1,15 +1,18 @@
-import java.util.*;
-public class CountNum {
+import java.util.Scanner;
+public class CountDig {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        System.out.println(countDigit(n));
+        Scanner sc = new Scanner(System.in);
+        System.out.print("n = ");
+        int n = sc.nextInt();
+        int ans = countDigit(n);
+        System.out.println(ans);
     }
     public static int countDigit(int n){
-        if(n == 0) return 1;   // edge case
-        n = Math.abs(n);
+        if ( n == 0){
+            return 1;
+        }
         int count = 0;
-        while(n>0){
+        while(n > 0){
             n = n / 10;
             count++;
         }
