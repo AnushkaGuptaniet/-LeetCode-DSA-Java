cf908bddfb4c14bc77b925fdffc301b146f34c7b
Anushka Gupta <anushkagupta4503@gmail.com>
2026-04-13 20:05:41 +0530

    SecondLargestElement

--- Arrays/SecondLargestElement.java
+++ Arrays/SecondLargestElement.java
@@ -1,15 +1,22 @@
-class Solution {
-    public int secondLargestElement(int[] nums) {
+public class SecondLargestElement {
+    public static void main(String[] args) {
+        int[] nums = {8,8,7,6,5};
         int n = nums.length;
-        int largest = nums[0], slargest = -1;
+        int result = secondLargestElement(nums, n);
+        System.out.print(result);
+    }
+    public static int secondLargestElement(int[] nums, int n){
+        int largest = 0;
+        int second_largest = nums[0];
         for(int i = 0; i < n; i++){
             if(nums[i] > largest){
-                slargest = largest;
+                second_largest = largest;
                 largest = nums[i];
-            }else if(nums[i] < largest && nums[i] > slargest){
-                slargest = nums[i];
+            }else if(nums[i] < largest && nums[i] > second_largest){
+                second_largest = nums[i];
             }
         }
-        return slargest;
+        return second_largest;
     }
 }
+// TC:O(n), SC:O(1)
