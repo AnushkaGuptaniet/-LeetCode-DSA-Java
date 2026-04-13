62550b14019ea996811b05359547d57f2446523a
Anushka Gupta <anushkagupta4503@gmail.com>
2026-04-13 22:12:50 +0530

    Deletion of the head of LL

--- Linked List/Delete node.java
+++ Linked List/Delete node.java
@@ -1,15 +1,37 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
-class Solution {
-    public void deleteNode(ListNode node) {
-        node.val = node.next.val;
+public class Problem237 {
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(4);
+        Node b = new Node(5);
+        Node c = new Node(1);
+        Node d = new Node(9);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        Node head = a;
+        Node node = b;
+        displayList(deleteNode(node, head));
+    }
+    public static Node deleteNode(Node node, Node head){
+        node.data = node.next.data;
         node.next = node.next.next;
+        return head;
+    }
+    public static void displayList(Node head){
+        Node curr = head;
+        while(curr != null){
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
     }
 }
-TC:- O(1), SC:- O(1)
+
+// TC:- O(1), SC:- O(1)
