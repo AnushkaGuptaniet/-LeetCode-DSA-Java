44c43642be96847803ddc4ea18712ecdbf314cc9
Anushka Gupta <anushkagupta4503@gmail.com>
2026-04-13 22:25:16 +0530

    Find length of a linked list

--- Linked List/find length of a linked list.java
+++ Linked List/find length of a linked list.java
@@ -1,44 +1,35 @@
-public class LinkedList2 {
-
-    public static class Node{
+public class GetLengthLL {
+    static class Node{
         int data;
         Node next;
         Node(int data){
             this.data = data;
+            this.next = null;
         }
-    }
-
-//    public static int length(Node head){
-//        Node temp = head;
-//        int count = 0;
-//        while(temp != null){
-//            count++;
-//            temp = temp.next;
-//        }
-//        return count;
-//    }
-
-    //Alternate approach
-    public static int length(Node head){
-        if(head == null){
-            return 0;
-        }
-        return 1 + length(head.next);
     }
 
     public static void main(String[] args) {
-        LinkedList2 ll = new LinkedList2();
-        Node a = new Node(5);
-        Node b = new Node(10);
-        Node c = new Node(15);
-        Node d = new Node(20);
-        Node e = new Node(25);
+        Node a = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
         a.next = b;
         b.next = c;
         c.next = d;
         d.next = e;
-        System.out.println(ll.length(a));
+        Node head = a;
+        int ans = getLength(head);
+        System.out.print(ans);
+    }
+    public static int getLength(Node head){
+        Node temp = head;
+        int count = 0;
+        while(temp != null){
+            count++;
+            temp = temp.next;
+        }
+        return count;
     }
 }
-
-
+// TC:_O(n), SC: - O(1)
