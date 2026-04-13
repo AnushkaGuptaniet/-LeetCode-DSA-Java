7665b82804c02dbd9a4da664a44479fdba41d797
Anushka Gupta <anushkagupta4503@gmail.com>
2026-04-13 19:49:49 +0530

    LargestElement

--- Arrays/LargestElement.java
+++ Arrays/LargestElement.java
@@ -1,12 +1,24 @@
-class Solution {
-    public int largestElement(int[] nums) {
-        int n = nums.length;
-        int maxNum = nums[0];
-        for(int i = 1; i < n; i++){
-            if(nums[i] > maxNum){
-                maxNum = nums[i];
+import java.util.*;
+public class LargestElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i = 0; i < n; i++){
+            nums[i] = sc.nextInt();
+        }
+        int ans = largestElement(nums, n);
+        System.out.println(ans);
+    }
+    public static int largestElement(int[] nums, int n){
+        int largest = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] > largest){
+                largest = nums[i];
             }
         }
-        return maxNum;
+        return largest;
     }
 }
+ // TC :O(n), SC:O(1)
