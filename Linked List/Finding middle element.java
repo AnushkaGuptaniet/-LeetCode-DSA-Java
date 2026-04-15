ebe6ef35e19d3b8c882ae702dd7540148735bfd5
Anushka Gupta <anushkagupta4503@gmail.com>
2026-04-15 10:58:58 +0530

    Finding middle element

--- Linked List/Finding middle element.java
+++ Linked List/Finding middle element.java
@@ -1,19 +1,9 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+public class Prob876 {
 
- // Approach 1:- Brute force 
-class Solution {
-    public ListNode middleNode(ListNode head) {
+
+    public static Node middleNode(Node head) {            // brute force
         int count = 0;
-        ListNode temp = head;
+        Node temp = head;
         while(temp != null){
             count++;
             temp = temp.next;
@@ -22,23 +12,54 @@ class Solution {
         for(int i = 0; i < count / 2; i++){
             temp = temp.next;
         }
-        return temp;    
+        return temp;
     }
-}
+
+    // TC:O(n), SC:_O(1)
+
+
+//    public static Node middleNode(Node head) {          // optimal - two pointer
+//        Node slow = head;
+//        Node fast = head;
+//        while(fast != null && fast.next != null){
+//            slow = slow.next;
+//            fast = fast.next.next;
+//        }
+//        return slow;
+//    }
 // TC:O(n), SC:_O(1)
 
 
-
- // Approach 2:- slow and fast pointer 
-class Solution {
-    public ListNode middleNode(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-        }   
-        return slow;
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
     }
+    public static void main(String[] args) {
+        Node a = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        Node head = a;
+        head = middleNode(head);
+        displayList(head);
+    }
+    public static void displayList(Node head){
+        Node curr = head;
+        while(curr != null){
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+    }
+
+
+
 }
-// TC:O(n), SC:_O(1)
