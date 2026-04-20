964ed1dbbe89dbb244a961da3787655f7c840f2a
Anushka Gupta <anushkagupta4503@gmail.com>
2026-04-20 09:58:24 +0530

    Linked List Cycle II

--- Linked List/Linked List Cycle II.java
+++ Linked List/Linked List Cycle II.java
@@ -1,52 +1,74 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
+import java.util.HashSet;
 
- // Approach 1:- brute force(hashing)
+public class Prob142 {
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static void main(String[] args) {
+        Node a = new Node(3);
+        Node b = new Node(2);
+        Node c = new Node(0);
+        Node d = new Node(-4);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = b;
+        Node head = a;
+        int pos = 1;
+        head = detectCycle(head);
+        if(head != null){
+            System.out.println("Cycle starts at node: " + head.data);
+        } else {
+            System.out.println("No cycle");
+        }
+    }
 
- public class Solution {
-    public boolean hasCycle(ListNode head) {
-        HashSet<ListNode> set = new HashSet<>();
-        ListNode temp = head;
-        while(temp != null){
+    // brute force
+    public static Node detectCycle(Node head){
+        Node temp = head;
+        HashSet<Node> set = new HashSet<>();
+        while(temp!=null){
             if(set.contains(temp)){
                 return temp;
             }
-            set.add(temp);
-            temp = temp.next;
-        }
-        return null;
-    }
- }
-
- // Approach 2 : - slow and fast pointer 
-public class Solution {
-    public ListNode detectCycle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-            if (slow == fast) {
-                ListNode entry = head;
-                while (entry != slow) {
-                    entry = entry.next;
-                    slow = slow.next;
-                }
-                return entry;
+            else{
+                set.add(temp);
+                temp = temp.next;
             }
         }
         return null;
     }
-}
 
-// TC:- O(n), sc:_O(1)
+    // TC: O(N), SC: O(n)
+
+
+    // optimized approach
+//    public static Node detectCycle(Node head){
+//        if(head == null){
+//            return null;
+//        }
+//        Node slow = head;
+//        Node fast = head;
+//        while(fast != null && fast.next != null){
+//            slow = slow.next;
+//            fast = fast.next.next;
+//            if(slow == fast){
+//                Node temp = head;
+//                while(temp != slow){
+//                    temp = temp.next;
+//                    slow = slow.next;
+//                }
+//                return temp;
+//            }
+//        }
+//        return null;
+//    }
+    // TC: O(N), SC: O(1)
+
+
+}
