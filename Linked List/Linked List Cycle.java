3cb82d52efa1faf934a29def6f2804da2b525708
Anushka Gupta <anushkagupta4503@gmail.com>
2026-04-20 09:34:09 +0530

    Linked List Cycle

--- Linked List/Linked List Cycle.java
+++ Linked List/Linked List Cycle.java
@@ -1,48 +1,62 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
+import java.util.HashSet;
 
- // Approach 1:- brute force(hashing)
-
- public class Solution {
-    public boolean hasCycle(ListNode head) {
-        HashSet<ListNode> set = new HashSet<>();
-        ListNode temp = head;
-        while(temp != null){
-            if(set.contains(temp)){
-                return true;
-            }
-            set.add(temp);
-            temp = temp.next;
+public class Prob141 {
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
         }
-        return false;
     }
- }
+    public static void main(String[] args) {
+        Node a = new Node(3);
+        Node b = new Node(2);
+        Node c = new Node(0);
+        Node d = new Node(-4);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = b;
+        Node head = a;
+        int pos = 1;
+        System.out.println(hasCycle(head));
+    }
+
+    // brute force
+//    public static boolean hasCycle(Node head){
+//        Node temp = head;
+//        HashSet<Node> set = new HashSet<>();
+//        while(temp!=null){
+//            if(set.contains(temp)){
+//                return true;
+//            }
+//            else{
+//                set.add(temp);
+//                temp = temp.next;
+//            }
+//        }
+//        return false;
+//    }
+
+    // TC: O(N), SC: O(n)
 
 
-
- Approach : - optimized(slow and fast pointer)
-public class Solution {
-    public boolean hasCycle(ListNode head) {
-        if (head == null) return false;
-
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while (fast != null && fast.next != null) {
+    // optimized approach
+    public static boolean hasCycle(Node head){
+        if(head == null){
+            return false;
+        }
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
-            if (slow == fast) return true;
+            if(slow == fast){
+                return true;
+            }
         }
         return false;
     }
+    // TC: O(N), SC: O(1)
 }
-TC:- O(N), SC:_O(1)
