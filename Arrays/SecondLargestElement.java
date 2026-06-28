f6caf9ce0d038689ccc254140d758fb63470515a
Anushka Gupta <anushkagupta4503@gmail.com>
2026-06-28 21:30:57 +0530

    SecondLargestElement.java

--- Arrays/SecondLargestElement.java
+++ Arrays/SecondLargestElement.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class SecondLargestElement {
     public static void main(String[] args) {
         int[] nums = {8,8,7,6,5};
@@ -5,18 +6,55 @@ public class SecondLargestElement {
         int result = secondLargestElement(nums, n);
         System.out.print(result);
     }
+    // Approach 1 : brute force (Sorting)
+   public static int secondLargestElement(int[] nums, int n){
+       Arrays.sort(nums);
+       int largest = nums[nums.length - 1];
+       for(int i = nums.length - 2; i >= 0; i--){
+           if(nums[i] != largest){
+               return nums[i];
+           }
+       }
+       return -1;
+   }
+    // TC: O(n log n), SC:O(log n)
+
+
+    //Approach 2 : Optimal but 2 traversals
+   public static int secondLargestElement(int[] nums, int n){
+       int largest = nums[0];
+       for(int i = 0; i < n; i++){
+           if(nums[i] > largest){
+               largest = nums[i];
+           }
+       }
+       int secondLargest = -1;
+       for(int i = 0; i < n; i++){
+           if(nums[i] != largest && nums[i] > secondLargest){
+               secondLargest = nums[i];
+           }
+       }
+       return secondLargest;
+   }
+    // TC: O(n), SC:O(1)
+
+
+    //Approach 3: optimal with one traversal
     public static int secondLargestElement(int[] nums, int n){
-        int largest = 0;
-        int second_largest = nums[0];
+        int largest = nums[0];
+        int secondLargest = -1;
+        if(n < 2){
+            return -1;
+        }
         for(int i = 0; i < n; i++){
             if(nums[i] > largest){
-                second_largest = largest;
+                secondLargest = largest;
                 largest = nums[i];
-            }else if(nums[i] < largest && nums[i] > second_largest){
-                second_largest = nums[i];
+            }else if(nums[i] != largest && nums[i] > secondLargest){
+                secondLargest = nums[i];
             }
         }
-        return second_largest;
+        return secondLargest;
     }
+    // TC:O(n), SC:O(1)
 }
-// TC:O(n), SC:O(1)
