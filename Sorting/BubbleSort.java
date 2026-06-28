e39cee8bf1d278aafeb07808c78175793f051641
Anushka Gupta <anushkagupta4503@gmail.com>
2026-06-28 13:27:01 +0530

    BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,56 +1,50 @@
-#Approach 1
-class Solution {
-    public int[] bubbleSort(int[] nums) {
-        int n = nums.length;
-        for(int i = 0; i < n; i++){
-            boolean flag = false;
-            for(int j = 0 ; j < n-i-1; j++){
-                if(nums[j] > nums[j+1]){
-                    swap(nums, j , j+1);
-                    flag = true;
-                }
-            }
-            if(!flag){
-                break;
-            }
-        }
-        return nums;
-
-    }
-    static void swap(int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
+public class bubbleSort {
+   public static void main(String[] args) {
+       int[] nums = { 7,4,1,5,3};
+       bubbleSort(nums);
+       for(int i = 0; i < nums.length; i++){
+           System.out.print(nums[i] + " ");
+       }
+   }
+   public static void bubbleSort(int[] nums){
+       int n = nums.length;
+       for(int i = 0; i < n-1 ; i++){
+           for(int j = 0; j < n-i-1; j++){
+               if(nums[j] > nums[j+1]){
+                   int temp = nums[j];
+                   nums[j] = nums[j+1];
+                   nums[j+1] = temp;
+               }
+           }
+       }
+   }
 }
-//Optimal Approach 
-class Solution {
 
-    public int[] bubbleSort(int[] nums) {
-
+// optimized version
+public class bubbleSort {
+    public static void main(String[] args) {
+        int[] nums = { 7,4,1,5,3};
+        bubbleSort(nums);
+        for(int i = 0; i < nums.length; i++){
+            System.out.print(nums[i] + " ");
+        }
+    }
+    public static void bubbleSort(int[] nums){
         int n = nums.length;
-
-        for(int i = 0; i < n-1; i++){
-
+        for(int i = 0; i < n-1 ; i++){
             boolean swapped = false;
-
             for(int j = 0; j < n-i-1; j++){
-
                 if(nums[j] > nums[j+1]){
-
                     int temp = nums[j];
                     nums[j] = nums[j+1];
                     nums[j+1] = temp;
-
                     swapped = true;
                 }
             }
-
-            if(swapped == false){
+            if(!swapped){
                 break;
             }
         }
-
-        return nums;
     }
 }
+// TC: O(n) for best case , O(n^2) for av and worst case , SC:O(1)
