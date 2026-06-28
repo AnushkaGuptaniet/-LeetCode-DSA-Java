b9fff11cc740758e25f3dc551c8e117fd2acca02
Anushka Gupta <anushkagupta4503@gmail.com>
2026-06-28 12:10:19 +0530

    Switch Case.java

--- Basics/Switch Case.java
+++ Basics/Switch Case.java
@@ -1,32 +1,32 @@
 import java.util.*;
-public class Solution {
+public class Whichweekday {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("day = ");
-        int day = input.nextInt();
+        int day = sc.nextInt();
         whichWeekDay(day);
     }
     public static void whichWeekDay(int day){
         switch(day){
-            case 1:
+            case 1 :
                 System.out.println("Monday");
                 break;
-            case 2:
+            case 2 :
                 System.out.println("Tuesday");
                 break;
-            case 3:
+            case 3 :
                 System.out.println("Wednesday");
                 break;
-            case 4:
+            case 4 :
                 System.out.println("Thursday");
                 break;
-            case 5:
+            case 5 :
                 System.out.println("Friday");
                 break;
-            case 6:
+            case 6 :
                 System.out.println("Saturday");
                 break;
-            case 7:
+            case 7 :
                 System.out.println("Sunday");
                 break;
             default:
@@ -34,3 +34,4 @@ public class Solution {
         }
     }
 }
+// TC : - O(1), SC: O(1)
